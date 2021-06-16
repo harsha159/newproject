1dc067e450b74dc5e839d30617481a00fd668c1b
harsha159 <79299161+harsha159@users.noreply.github.com>
2021-06-16 16:46:40 +0530

    Update strringreverse.java

--- strringreverse.java
+++ strringreverse.java
@@ -8,16 +8,12 @@ public class strringreverse {
    
    String s="harsha";
    
-// int i= s.length()-1;
- 
-  //  for(i=s;i>=0;i++)
-   
    
 	for(int i=s.length()-1;i>=0;i--)
 	{
 	
 	   System.out.print(s.charAt(i));
-   }//
+	}
 	
 }
 }
